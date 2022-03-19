75f52a885d3a35cccdaaaffce0c3b142a1c59f64
Fabrice Folituu <fabrice.folituu@lwane.com>
2022-03-18 16:07:14 -1000

    Create Event Add

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,46 @@
-import { Col, Container, Row, ListGroup, Alert, Badge } from 'react-bootstrap'
+import React from 'react'
 import logo from './assets/img/favicon.svg'
 import './assets/css/app.css'
+import { Col, Container, Row, ListGroup, Alert, Badge } from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCloudSun, faMedal, faFlag } from '@fortawesome/free-solid-svg-icons'
 
 const App: React.FunctionComponent<any> = () => {
+  // State
+  const [todoList, setTodoList] = React.useState<any[]>([])
+  const [inputTask, setInputTask] = React.useState<string>('')
+
+  // Init
+  const getColorUrgent = (param: boolean) => {
+    return param ? 'danger' : 'muted'
+  }
+
+  // Event
+  const handleChange = (e: React.FormEvent<HTMLInputElement>) => {
+    setInputTask(e.currentTarget.value)
+  }
+
+  const handleSubmit = (e: React.SyntheticEvent) => {
+    e.preventDefault()
+    const newTask = { id: Math.random(), name: inputTask, urgent: false, completed: false }
+    setTodoList([...todoList, newTask])
+    setInputTask('')
+  }
+
+  const handleToggleUrgent = (todo: object) => {
+    //console.log(todo)
+    //const index = todoList.findIndex((task) => task.id === todo.id)
+    //const newTodoList = [...todoList]
+
+    //console.log(index)
+    //console.log(newTodoList)
+
+    //newTodoList[index] = { id: id, name: inputValueEdit.name }
+    //setList(newList)
+    //setInputValueEdit({ name: '' })
+  }
+
+  // Render
   return (
     <Container fluid className='app'>
       <Row>
@@ -18,19 +54,22 @@ const App: React.FunctionComponent<any> = () => {
               <FontAwesomeIcon icon={faCloudSun} fontSize={20} className='me-2 text-primary' />
               Ma journée
               <Badge pill className='float-end'>
-                3
+                {todoList.length}
               </Badge>
             </ListGroup.Item>
             <ListGroup.Item>
               <FontAwesomeIcon icon={faFlag} fontSize={20} className='me-2 text-danger' />
               Important
               <Badge bg='danger' pill className='float-end'>
-                2
+                {todoList.filter((item) => item.urgent === true).length}
               </Badge>
             </ListGroup.Item>
             <ListGroup.Item>
               <FontAwesomeIcon icon={faMedal} fontSize={20} className='me-2 text-success' />
               Terminé
+              <Badge bg='success' pill className='float-end'>
+                {todoList.filter((item) => item.completed === true).length}
+              </Badge>
             </ListGroup.Item>
           </ListGroup>
         </Col>
@@ -45,40 +84,27 @@ const App: React.FunctionComponent<any> = () => {
             </small>
           </div>
           <div className='todolist'>
-            <Alert>
-              <input type='radio' className='me-2' /> Mettre à jour le template de mail pour la
-              zone de livraison Cleveron
-              <FontAwesomeIcon
-                icon={faFlag}
-                fontSize={17}
-                className='me-2 float-end text-danger'
-              />
-            </Alert>
-            <Alert>
-              <input type='radio' className='me-2' /> Tester toute la liste des Erreurs de
-              Cleveron
-              <FontAwesomeIcon
-                icon={faFlag}
-                fontSize={17}
-                className='me-2 float-end text-danger'
-              />
-            </Alert>
-            <Alert>
-              <input type='radio' className='me-2' /> Ne pas oublier d'appeler Vaiana pour
-              Teanu
-              <FontAwesomeIcon
-                icon={faFlag}
-                fontSize={17}
-                className='me-2 float-end text-muted'
-              />
-            </Alert>
+            {todoList.map((todo: any) => (
+              <Alert key={todo.id}>
+                <input type='radio' className='me-2' /> {todo.name}
+                <FontAwesomeIcon
+                  icon={faFlag}
+                  fontSize={17}
+                  className={`me-2 float-end text-${getColorUrgent(todo.urgent)}`}
+                  style={{ cursor: 'pointer' }}
+                  onClick={() => handleToggleUrgent(todo)}
+                />
+              </Alert>
+            ))}
           </div>
           <div className='form-add-todo'>
-            <form>
+            <form onSubmit={handleSubmit}>
               <input
                 type='text'
                 className='form-control'
                 placeholder='Ajouter une nouvelle tâche'
+                value={inputTask}
+                onChange={handleChange}
               />
             </form>
           </div>
